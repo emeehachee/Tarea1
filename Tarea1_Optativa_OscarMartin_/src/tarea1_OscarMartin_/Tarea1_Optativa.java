02a4eae5af17aba85a654c3398919b1230640ca6
emeehachee <lolowie24@gmail.com>
2025-10-08 10:23:44 +0200

    Update Tarea1_Optativa.java

--- Tarea1_Optativa_OscarMartin_/src/tarea1_OscarMartin_/Tarea1_Optativa.java
+++ Tarea1_Optativa_OscarMartin_/src/tarea1_OscarMartin_/Tarea1_Optativa.java
@@ -5,6 +5,6 @@ public class Tarea1_Optativa {
 	public static void main (String[] args) {
 	
 	System.out.println("Primera tarea de la optativa");
-
+	System.out.println("Cambio realizados desde github");
 }
 }
